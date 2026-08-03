54e50615f2d19e76c35a6e73c6cb466edcf85f51
Pedro Gadelha <pgpgadelha123@gmail.com>
2026-08-03 14:54:30 -0300

    feat(backend): modelar usuario e ligar relacao com pedido

---
+++ backend/prisma/migrations/20260803175153_add_usuario/migration.sql
@@ -0,0 +1,21 @@
+-- CreateTable
+CREATE TABLE `Usuario` (
+    `id` INTEGER NOT NULL AUTO_INCREMENT,
+    `nome` VARCHAR(191) NOT NULL,
+    `email` VARCHAR(191) NOT NULL,
+    `senha` VARCHAR(191) NOT NULL,
+    `role` ENUM('CLIENTE', 'FUNCIONARIO', 'ADMIN') NOT NULL DEFAULT 'CLIENTE',
+    `ativo` BOOLEAN NOT NULL DEFAULT true,
+    `rua` VARCHAR(191) NULL,
+    `complemento` VARCHAR(191) NULL,
+    `estado` VARCHAR(191) NULL,
+    `cep` VARCHAR(191) NULL,
+    `createdAt` DATETIME(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3),
+    `updatedAt` DATETIME(3) NOT NULL,
+
+    UNIQUE INDEX `Usuario_email_key`(`email`),
+    PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- AddForeignKey
+ALTER TABLE `Pedido` ADD CONSTRAINT `Pedido_usuarioId_fkey` FOREIGN KEY (`usuarioId`) REFERENCES `Usuario`(`id`) ON DELETE RESTRICT ON UPDATE CASCADE;
