88d691e384ec8f70f6a1b4c3b82809c7751c0a05
Pedro Gadelha <pgpgadelha123@gmail.com>
2026-08-04 11:36:08 -0300

    feat(backend): criar service de login de usuario

--- backend/src/services/auth.service.ts
+++ backend/src/services/auth.service.ts
@@ -1,11 +1,12 @@
 import bcrypt from 'bcryptjs';
+import jwt from "jsonwebtoken";
 import { prisma } from '../lib/prisma';
 import { AppError } from '../errors/app-error';
 
 export async function register(name: string, email: string, password: string) {
     const existingUser = await prisma.usuario.findUnique({ where: { email } });
 
-    if (existingUser) {
+    if (existingUser){
         throw new AppError('Email já cadastrado', 409);
     }
 
@@ -17,4 +18,32 @@ export async function register(name: string, email: string, password: string) {
     });
 
     return user;
+}
+
+export async function login(email: string, password: string){
+    const user = await prisma.usuario.findUnique({ where: { email } });
+
+    if (!user){
+        throw new AppError('Credenciais inválidas!', 401);
+    }
+
+    if(!user.ativo){
+        throw new AppError('Usuário desabilitado' , 403);
+    }
+
+    const comparePassoword = await bcrypt.compare(password , user.senha);
+
+    if(!comparePassoword){
+        throw new AppError('Credenciais inválidas!' , 401);
+    }
+
+    const token = jwt.sign({userId: user.id, role: user.role}, 
+            process.env.JWT_SECRET!, {expiresIn: '15m'});
+
+    return{token, user:{
+        id: user.id,
+        name: user.nome,
+        email: user.email,
+        role: user.role
+    }};
 }
