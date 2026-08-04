d45c67b4aaef19aea39cdda08fa82494250936cc
Pedro Gadelha <pgpgadelha123@gmail.com>
2026-08-04 11:18:24 -0300

    feat(backend): criar app.ts com tratamento global de erros

---
+++ backend/src/app.ts
@@ -0,0 +1,36 @@
+import 'dotenv/config';
+import express, {Request, Response, NextFunction} from 'express';
+import cors from 'cors';
+import { ZodError } from 'zod';
+import { JsonWebTokenError, TokenExpiredError } from 'jsonwebtoken';
+import { AppError } from './errors/app-error';
+import authRoutes from './routes/auth.route';
+
+const app = express();
+
+app.use(express.json());
+app.use(cors());
+
+app.use('/auth' , authRoutes);
+
+app.use((error: Error, request: Request, response: Response, next: NextFunction) => {
+    if(error instanceof ZodError){
+        return response.status(400).json({
+            message: 'Dados inválidos', 
+            issues: error.issues
+        });
+    }
+
+    if(error instanceof JsonWebTokenError || error instanceof TokenExpiredError){
+        return response.status(401).json({message: 'token inválido ou expirado'});
+    }
+
+    if(error instanceof AppError){
+        return response.status(error.statusCode).json({message: 'Erro interno do servidor'});
+    }
+
+    console.error(error);
+    response.status(500).json({message: 'Erro interno do servidor'});
+});
+
+export default app;
