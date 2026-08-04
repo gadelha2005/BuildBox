55ca0ae88553cbc68308df0f1838a906fa8e8d1b
Pedro Gadelha <pgpgadelha123@gmail.com>
2026-08-04 17:44:23 -0300

    feat(backend): criar middleware de autenticacao jwt

---
+++ backend/src/middlewares/auth.middlware.ts
@@ -0,0 +1,25 @@
+import {Request, Response, NextFunction} from 'express';
+import jwt from 'jsonwebtoken';
+import {AppError} from '../errors/app-error';
+
+interface TokenPayload{
+    userId: number;
+    role: string;
+}
+
+export function authMiddleware(request: Request, response: Response, next: NextFunction){
+    const authHeader = request.headers.authorization;
+
+    if(!authHeader){
+        throw new AppError('Token não fornecido' , 401);
+    }
+
+    const [, token] = authHeader.split('');
+
+    const payload = jwt.verify(token, process.env.JWT_SECRET!) as TokenPayload;
+
+    request.user = payload;
+
+    next();
+}
+
