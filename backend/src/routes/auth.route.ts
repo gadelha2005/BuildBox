867ce05c742735cef61303254f150f6122bc9bf9
Pedro Gadelha <pgpgadelha123@gmail.com>
2026-08-04 11:36:58 -0300

    feat(backend): criar rota de login de usuario

--- backend/src/routes/auth.route.ts
+++ backend/src/routes/auth.route.ts
@@ -4,5 +4,5 @@ import * as authController from '../controllers/auth.controller';
 const router = Router();
 
 router.post('/register' , authController.register);
-
+router.post('/login', authController.login);
 export default router;
